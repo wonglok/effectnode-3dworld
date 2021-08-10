64734ce66bae527e01ed9e9e170921ab39e1363f
wonglok <yellowhappy831@gmail.com>
2021-08-10 09:50:39 +0800

    Updates

---
+++ precache-manifest.d5231b27eadd7c3ac6c779254962cfba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e18336b58d216fec0e51d38d4b07a77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0505bdff826a8661514b",
+    "url": "./static/css/main.bf48cada.chunk.css"
+  },
+  {
+    "revision": "9291db92659551490e65",
+    "url": "./static/js/2.3ef2adaa.chunk.js"
+  },
+  {
+    "revision": "dce56323f310cc45b5e17690fd81342d",
+    "url": "./static/js/2.3ef2adaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0505bdff826a8661514b",
+    "url": "./static/js/main.e364578a.chunk.js"
+  },
+  {
+    "revision": "2847a7829cc476fcb5b3",
+    "url": "./static/js/runtime-main.410e397d.js"
+  }
+]);
