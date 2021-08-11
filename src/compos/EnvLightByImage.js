7c894b7dc76bea98f50ae7fa216d76efa034f548
wonglok <yellowhappy831@gmail.com>
2021-08-12 06:22:08 +0800

    lets go

--- src/compos/EnvLightByImage.js
+++ src/compos/EnvLightByImage.js
@@ -1,29 +1,26 @@
+import { useTexture } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 import { useEffect } from 'react'
 
-import { PMREMGenerator, sRGBEncoding, TextureLoader } from 'three'
+import { PMREMGenerator, sRGBEncoding } from 'three'
 
 export function EnvLightByImage({ imageURL }) {
-  //
   let { scene, gl } = useThree()
+  let texture = useTexture(imageURL)
   useEffect(() => {
     const pmremGenerator = new PMREMGenerator(gl)
     pmremGenerator.compileEquirectangularShader()
 
-    let loader = new TextureLoader()
-    // loader.setDataType(UnsignedByteType);
-    loader.load(imageURL, (texture) => {
-      const envMap = pmremGenerator.fromEquirectangular(texture).texture
-      envMap.encoding = sRGBEncoding
-      // scene.background = envMap;
-      scene.environment = envMap
-    })
+    const envMap = pmremGenerator.fromEquirectangular(texture).texture
+    envMap.encoding = sRGBEncoding
+    // scene.background = envMap;
+    scene.environment = envMap
 
     return () => {
       scene.environment = null
       scene.background = null
     }
-  }, [imageURL])
+  }, [])
 
   return null
 }
