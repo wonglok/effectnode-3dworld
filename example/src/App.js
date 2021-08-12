ca6c9df09deed324cdcc27a24d88c60cfbb57ba8
Wong Lok <wonglok@users.noreply.github.com>
2021-08-12 14:38:36 +0800

    app

--- example/src/App.js
+++ example/src/App.js
@@ -14,6 +14,7 @@ import {
 import { SkeletonUtils } from 'three/examples/jsm/utils/SkeletonUtils'
 import { Vector3 } from 'three'
 
+//
 // needs trailing slash
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
