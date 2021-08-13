c3f77872e0a2664dfbd8b5c765f77796c1640354
wonglok <yellowhappy831@gmail.com>
2021-08-13 09:02:36 +0800

    Updates

--- precache-manifest.fd945f5fa6e01bac2c0388c8fb5aa6fe.js
+++ precache-manifest.fd945f5fa6e01bac2c0388c8fb5aa6fe.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "958765fc1c6eae442469e409c44b3173",
+    "revision": "2ffe53eed2374ad3c84e51a9ce9955ea",
     "url": "./index.html"
   },
   {
-    "revision": "55052d4a216978964541",
+    "revision": "a7a39efd5c194318851a",
     "url": "./static/css/main.bf48cada.chunk.css"
   },
   {
-    "revision": "ccc64e598cf620fe4b59",
-    "url": "./static/js/2.9be5c229.chunk.js"
+    "revision": "0bd763399998afe06e9a",
+    "url": "./static/js/2.0318c19b.chunk.js"
   },
   {
     "revision": "d9fb80168123e0a96eab6174dd7ca7af",
-    "url": "./static/js/2.9be5c229.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.0318c19b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "55052d4a216978964541",
-    "url": "./static/js/main.b1c35bf4.chunk.js"
+    "revision": "a7a39efd5c194318851a",
+    "url": "./static/js/main.bab37ef9.chunk.js"
   },
   {
     "revision": "2847a7829cc476fcb5b3",
